4059d9b3c8f9517ce7a128ceab75ef4ca88d944a
RAGHAV THAKKAR <thakkarraghav2906@gmail.com>
2019-07-07 09:49:16 +0530

    init commit

---
+++ android/app/src/main/kotlin/com/avangers/flutter/flutter_hackthon/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.avangers.flutter.flutter_hackthon
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
